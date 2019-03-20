49165a8ab6b9ac5bb32f2b3cc84a6bb8e757af63
Li <c_li70655@tss-hr420-dmspr3.fc.ca>
2019-03-20 12:04:57 -0400

    22

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    pword: "root",
+    host: "localhost",
+    port: "8889", //window :3306 
+    database: "DB-FB"
+}
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var connect = require('../utils/sqlConnect');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  //
+  connect.query('SELECT name, avatar FROM  hero', (err, result) =>{
+    if (err) {
+      throw err;
+      console.log(err);
+    }else{
+      console.log(result);
+      // res.render('index', { avatars: result });
+    }
+  });
+
+});
+
+module.exports = router;
